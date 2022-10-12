c020695c900c52a49757df8a74bfabd6dd13e3fb
salmanfarsy <blieveburn007@gmail.com>
2022-10-12 20:46:34 +0600

    added some changes

--- english/main.js
+++ english/main.js
@@ -5,6 +5,8 @@ const correctVideo = document.querySelector('.correctVideo');
 const wrongVideo = document.querySelector('.wrongVideo');
 const input = document.querySelector('input');
 const submit = document.querySelector('.submit');
+const tryAgain = document.querySelector('.try');
+const label = document.querySelector('label');
 
 //events
 startCall.addEventListener('click', ()=>{
@@ -12,23 +14,36 @@ startCall.addEventListener('click', ()=>{
     input.classList.remove('off')
     submit.classList.remove('off')
     startCall.classList.add('off')
+    label.classList.remove('off')
 })
 
 submit.addEventListener('click', ()=>{
     const code = input.value;
     startCall.classList.add('off')
     firstVideo.classList.add('off')
+    label.classList.add('off')
     input.classList.add('off')
     submit.classList.add('off')
     firstVideo.pause();
     if(parseInt(code) === 1394){
-   
+        wrongVideo.pause();
+        wrongVideo.classList.add('off') 
         correctVideo.classList.remove('off')
         
     }
     else
     {
-     
+        label.classList.add('off')
         wrongVideo.classList.remove('off') 
+        tryAgain.classList.remove('off') 
+
     }
+})
+
+tryAgain.addEventListener('click', ()=>{
+    label.classList.remove('off')
+    input.classList.remove('off')
+    input.value = ''
+    submit.classList.remove('off')
+    tryAgain.classList.add('off') 
 })
